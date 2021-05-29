35fc38b21a337b675b68c409fe2e16c0a7c24ee1
Azizbek <azizbeksavkimov@mail.ru>
2021-05-29 11:28:05 +0500

    fix: bags

--- src/components/shared/isValidEmail.ts
+++ src/components/shared/isValidEmail.ts
@@ -1,14 +1,15 @@
 const emailLength = 30;
 
+const emailValidation =
+  // eslint-disable-next-line max-len
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 export function isValidEmail(value: string): string | boolean {
   const errors = [
     'Email cannot be empty',
     'Must comply with the standard email generation rule ',
     `The email of characters must be less than ${emailLength} characters`,
   ];
-
-  const emailValidation =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   if (value === '') {
     return errors[0];
